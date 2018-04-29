0f06a37063e65014ac560ea40d979c7ac9a56752
xnjd-lbs <31677256+xnjd-lbs@users.noreply.github.com>
2018-04-29 16:22:25 +0800

    core包依赖解决

--- eatcrabs/polyFinance-eatcrabs-desk-web/src/main/java/com/ptteng/DeskApplication.java
+++ eatcrabs/polyFinance-eatcrabs-desk-web/src/main/java/com/ptteng/DeskApplication.java
@@ -2,12 +2,12 @@ package com.ptteng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.ptteng.controller"})
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class,DataSourceAutoConfiguration.class})
 public class DeskApplication extends SpringBootServletInitializer {
 
     @Override
