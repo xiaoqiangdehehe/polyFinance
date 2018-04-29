f00dc0622951f03c94ad108bef1f04454ff43cee
xiaoqiangdehehe <1191661688@qq.com>
2018-04-29 11:43:51 +0800

    domain

---
+++ eatcrabs/polyFinance-eatcrabs-desk-web/src/main/java/com/ptteng/domain/manager/Module.java
@@ -0,0 +1,74 @@
+package com.ptteng.domain.manager;
+
+/**
+ * 模块表-manager
+ */
+public class Module {
+    private Long id;
+    private String name;
+    private Long fmoduleId;
+    private String url;
+    private Long createAt;
+    private Long updateAt;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setFmoduleId(Long fmoduleId) {
+        this.fmoduleId = fmoduleId;
+    }
+
+    public Long getFmoduleId() {
+        return fmoduleId;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUpdateAt(Long updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Long getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setCreateAt(Long createAt) {
+        this.createAt = createAt;
+    }
+
+    public Long getCreateAt() {
+        return createAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Module{" +
+                "id = " + id +
+                ", name = " + name +
+                ", fmoduleId = " + fmoduleId +
+                ", url = " + url +
+                ", createAt = " + createAt +
+                ", upeateAt = " + updateAt +
+                "}";
+    }
+
+}
